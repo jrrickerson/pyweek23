d3ca3c50cac232727a7d3f047da7fcc5b01a625b
JR Rickerson <jrrickerson@gmail.com>
2017-02-20 22:52:21 -0500

    Run the basic game test.
    Import appropriate objects from the new packages and put them together
    to start the game.

--- src/main.py
+++ src/main.py
@@ -1,2 +1,15 @@
+import sge
+from sge.gfx import Background, Color
+from .game import Game
+from .rooms import MainMenu
+from .objects.testing import TestPlayer
+
+
 def main():
-    # program
+    sge.game = Game(width=640, height=480, fps=120, window_text='SGE Test')
+    background = sge.gfx.Background([], sge.gfx.Color("black"))
+    player = TestPlayer(sge.game.width / 2, sge.game.height / 2)
+    sge.game.start_room = MainMenu(
+        objects=[player], background=background)
+    sge.game.mouse_visible = False
+    sge.game.start()
